14465541ea161d2b177c680b1ad5d3bf7f1a13d1
Em Fhal <e@fhal.org>
2022-04-02 22:07:09 +0300

    BASE-1/add-mysql-user ver2

--- src/main/java/com/emdemo/spring/data/news/StartDashboard.java
+++ src/main/java/com/emdemo/spring/data/news/StartDashboard.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartDashboard {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StartDashboard.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StartDashboard.class, args);
+    }
 
 }
